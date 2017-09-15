9f29b8b3d2ea4fb1ee25f71622f04e453739e1da
ANTHONIESU_HOME\Anthonie Su <niewolf@gmail.com>
2017-09-15 15:15:38 +0800

    1. Try to unzip multiple zips due to maybe there's subtitle zip. 2. Support public password txt. 3. Need to remove files after all zips unzip.

--- UnzipFiles.py
+++ UnzipFiles.py
@@ -14,17 +14,29 @@ except:
 
 rarfile.UNRAR_TOOL='UnRAR.exe'
 
+
+zip_name_list = []
+pwd_list = []
 def decryptRarZipFile(rootdir, filename):
 
-    if filename.endswith('.zip'):
-        fp = zipfile.ZipFile(filename)
-    elif filename.endswith('.rar'):
-        fp = rarfile.RarFile(filename)
+    try:
+        if filename.endswith('.zip'):
+            fp = zipfile.ZipFile(filename)
+        elif filename.endswith('.rar'):
+            fp = rarfile.RarFile(filename)
+    except:
+        return
 
     #the destination path of zipfile
     desPath = filename[:-4]
     if not os.path.exists(desPath):
         os.mkdir(desPath)
+
+    if fp.namelist in zip_name_list:
+        return
+
+    zip_name_list.append(fp.namelist)
+    print ("unzip " + filename)
     #try to use non-password to unzip.
     try:
         fp.extractall(desPath)
@@ -37,39 +49,54 @@ def decryptRarZipFile(rootdir, filename):
             password_path = os.path.join(rootdir, '新文字文件.txt')
             if os.path.exists(password_path):
                 fpPwd = open(password_path, encoding = 'UTF-8-sig')
+                for pwd in fpPwd:
+                    pwd = pwd.rstrip()
+                    pwd_list.append(pwd)
+                fpPwd.close()
+
         except:
             print('No dict file in current directory.')
-            return
-        for pwd in fpPwd:
-            pwd = pwd.rstrip()
+        
+        for check_pwd in pwd_list:
             try:
-                print ("unzip " + filename + "...")
                 if filename.endswith('.zip'):
                     for file in fp.namelist():
-                        fp.extract(file, path=desPath, pwd=pwd.encode())
+                        fp.extract(file, path=desPath, pwd=check_pwd.encode())
                         os.rename(desPath+'\\'+file, desPath+'\\'+file.encode('cp437').decode('gbk'))
-                    print('Success! ====>'+pwd)
+                    print('Success! ====>'+check_pwd)
                     fp.close()
                     break
                 elif filename.endswith('.rar'):
-                    fp.extractall(path=desPath, pwd=pwd)
-                    print('Success! ====>'+pwd)
+                    fp.extractall(path=desPath, pwd=check_pwd)
+                    print('Success! ====>'+check_pwd)
                     fp.close()
                     break
             except:
                 pass
-        fpPwd.close()
 
 download_folder = "K:\\下載\\"
 
+def get_public_pwd ():
+    public_pwd_path = "C:\\Users\\Anthonie Su\\Desktop\\password.txt"
+    if os.path.exists(public_pwd_path):
+        fp_public_pwd = open(public_pwd_path, encoding = 'UTF-8-sig')
+        for public_pwd in fp_public_pwd:
+            public_pwd = public_pwd.rstrip()
+            pwd_list.append(public_pwd)
+
 if __name__ == '__main__':
 
+    get_public_pwd ()
+    
     for root, dirs, files in os.walk(download_folder):
-        unzip = False
         for file in files:
             filename = os.path.join(root, file)
             if os.path.isfile(filename) and filename.endswith(('.zip', '.rar')):
-                if unzip == False:
-                    decryptRarZipFile(root, filename)
-                    unzip = True
+                decryptRarZipFile(root, filename)
+
+    for root, dirs, files in os.walk(download_folder):
+        for file in files:
+            filename = os.path.join(root, file)
+            if os.path.isfile(filename) and filename.endswith(('.zip', '.rar')):
                 os.remove(filename)
+      
