805c3e88eef9241ea1c62a5c5118cf72d2b3d959
ANTHONIESU_HOME\Anthonie Su <niewolf@gmail.com>
2017-09-20 13:04:28 +0800

    Prevent to unzip with other part of zips.

--- UnzipFiles.py
+++ UnzipFiles.py
@@ -14,6 +14,13 @@ except:
 
 rarfile.UNRAR_TOOL='UnRAR.exe'
 
+def mkdir_dest(desPath):
+    if not os.path.exists(desPath):
+        os.mkdir(desPath)
+
+def rmdir_dest(desPath):
+    if os.path.exists(desPath):
+        os.rmdir(desPath)
 
 zip_name_list = []
 pwd_list = []
@@ -24,27 +31,24 @@ def decryptRarZipFile(rootdir, filename):
             fp = zipfile.ZipFile(filename)
         elif filename.endswith('.rar'):
             fp = rarfile.RarFile(filename)
-    except:
-        return False
+    except Exception as e:
+        return False, ""
 
     #the destination path of zipfile
     desPath = filename[:-4]
-    if not os.path.exists(desPath):
-        os.mkdir(desPath)
-
-    if fp.namelist in zip_name_list:
-        return False
-
-    zip_name_list.append(fp.namelist)
+    mkdir_dest(desPath)
+    
     print ("unzip " + filename)
     #try to use non-password to unzip.
     try:
+        #mkdir_dest(desPath)
         fp.extractall(desPath)
         fp.close()
         print('No password')
-        return True
+        return True, filename
     #use dictionary to unzip.
     except:
+        #rmdir_dest(desPath)
         try:
             password_path = os.path.join(rootdir, '新文字文件.txt')
             if os.path.exists(password_path):
@@ -60,21 +64,31 @@ def decryptRarZipFile(rootdir, filename):
         for check_pwd in pwd_list:
             try:
                 if filename.endswith('.zip'):
+                    #mkdir_dest(desPath)
                     for file in fp.namelist():
                         fp.extract(file, path=desPath, pwd=check_pwd.encode())
                         os.rename(desPath+'\\'+file, desPath+'\\'+file.encode('cp437').decode('gbk'))
                     print('Success! ====>'+check_pwd)
                     fp.close()
-                    return True
+                    return True, filename
                     break
                 elif filename.endswith('.rar'):
+                    #fp.setpassword(check_pwd)
+                    #if fp.namelist() in zip_name_list:
+                    #    os.rmdir(desPath)
+                    #    return False
+                    #mkdir_dest(desPath)
                     fp.extractall(path=desPath, pwd=check_pwd)
+                    #zip_name_list.append(fp.namelist())
                     print('Success! ====>'+check_pwd)
                     fp.close()
-                    return True
+                    return True, filename
                     break
             except Exception as e:
-                print(e)
+                #try:
+                #    rmdir_dest(desPath)
+                #except:
+                #    pass
                 pass
 
 download_folder = "K:\\下載\\"
@@ -93,10 +107,13 @@ if __name__ == '__main__':
     
     for root, dirs, files in os.walk(download_folder):
         unzipped = False
+        unzipName = ""
         for file in files:
             filename = os.path.join(root, file)
+            if filename[:-7] == unzipName[:-7]:
+                break
             if os.path.isfile(filename) and filename.endswith(('.zip', '.rar')):
-                ret = decryptRarZipFile(root, filename)
+                ret, unzipName = decryptRarZipFile(root, filename)
                 if (ret == True):
                     unzipped = ret
         for file in files:
